2683259ce0d6b29e2f0118573ed373558bf844b5
Fernanda <fernandamora88@gmail.com>
2017-04-13 09:38:28 -0700

    finish linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Link
   attr_accessor :key, :val, :next, :prev
 
@@ -13,13 +15,24 @@ class Link
   end
 
   def remove
+    @prev.next = @next
+    @next.prev = @prev
+    @next = nil
+    @prev = nil
+    self
     # optional but useful, connects previous link to next link
     # and removes self from list.
   end
 end
 
 class LinkedList
+  include Enumerable
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,34 +41,55 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail
   end
 
   def get(key)
+    each { |link| return link.val if link.key == key }
+    nil
   end
 
   def include?(key)
+    any? { |link| link.key == key }
   end
 
   def append(key, val)
+    link = Link.new(key, val)
+    @tail.prev.next = link
+    link.prev = @tail.prev
+    link.next = @tail
+    @tail.prev = link
+    link
   end
 
   def update(key, val)
+    each { |link| link.val = val if link.key == key }
   end
 
   def remove(key)
+    each { |link| return link.remove.val if link.key == key }
+
+    nil
   end
 
-  def each
+  def each(&prc)
+    link = @head.next
+    until link == @tail
+      prc.call(link)
+      link = link.next
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
-  # def to_s
-  #   inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
-  # end
+  def to_s
+    inject([]) { |acc, link| acc << "[#{link.key}, #{link.val}]" }.join(", ")
+  end
 end
