ac04eb36938ecfc1e837a6e0fc0c54d3f6bb0da2
Fernanda <fernandamora88@gmail.com>
2017-04-12 21:21:46 -0700

    finish hashing functions

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    result = 0
+    each_with_index do |element, index|
+      result = (element.hash + index.hash) ^ result
+    end
+    result
   end
 end
 
 class String
   def hash
+    result = 0
+    chars.each_with_index do |element, index|
+      result = (element.ord.hash + index.hash) ^ result
+    end
+    result
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    result = 0
+    each do |key, value|
+      result = (key.to_s.hash + value.hash) ^ result
+    end
+    result
   end
 end
